b566395c0a35d241e81dbe1003a0fe2225c1169c
DanielJairton <daniel.jairton679@gmail.com>
2023-12-30 19:56:19 -0300

    Consumindo API Github com REACT - versão final

--- Api Github/src/App.jsx
+++ Api Github/src/App.jsx
@@ -2,33 +2,73 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
+
 function App() {
   const [usuario, setUsuario] = useState("")
   function handleBuscar(){
 
-    axios.get(`https://api.github.com/users/${usuario}`).then(response => console.log(response.data))
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data))
-    // let dados = axios.get(`https://api.github.com/users/${usuario}`).then(response => response.json())
-    // console.log(dados)
+    // axios.get(`https://api.github.com/users/${usuario}`).then(response => console.log(response.data))
+    // axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data))
+
+    buscarDados(usuario);
+  }
+
+  //Busca os dados da Api Github para pegar os dados do usuário
+  async function buscarDados(usuario){
+    let dados = await fetch(`https://api.github.com/users/${usuario}`).then(Response => Response.json())
+    let repos = await fetch(`https://api.github.com/users/${usuario}/repos`).then(Response => Response.json())
+    inserirDados(dados, repos);
+  }
+
+  //Inseri os dados do usuário
+  function inserirDados(dados, repos){
+    document.getElementById('lb-usuario').innerHTML = dados.login;
+    document.getElementById('lb-usuario').style.visibility = "visible";
+    document.getElementById('img-perfil').src = dados.avatar_url;
+    document.getElementById('img-perfil').style.visibility = "visible";
+    criarListaRepos(repos);
+  }
+
+  function criarListaRepos(repos){
+
+    //Remove repositórios da pesquisa antigua da lista
+    let removerLista = document.querySelectorAll('.li-repo');
+    for (let index = 0; index < removerLista.length; index++) {
+      document.getElementById('ul-repos').removeChild(removerLista[index]); 
+    }
+
+    //adiciona os repositórios do usuário na lista
+    for (let index = 0; index < repos.length; index++) {
+      let li = document.createElement('li');
+      li.innerHTML = "<a href=" + repos[index].html_url + " target='_blank'>" + repos[index].name + "</a>";
+      li.className = "li-repo";
+
+      document.getElementById('ul-repos').appendChild(li);
+    }
   }
 
   return (
     <>
-      {/* <img src="https://avatars.githubusercontent.com/u/152343755?v=4" alt="avtar" /> */}
-      {/* <img src="res" alt="avatar"/> */}
       <div className='box-maior'>
+
         <div className='box-input'>
           <input type="text" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-          <button onClick={handleBuscar}>Pesquisar</button>
+          <button onClick={handleBuscar} id='btn-buscar'>Pesquisar</button>
         </div>
 
         <div className='box-perfil'>
-          <img src="" alt="img-perfil" />
-          <label htmlFor="">Login</label>
+          <img src="" alt="img-perfil" id='img-perfil'/>
+          <label htmlFor="" id='lb-usuario'>Login</label>
         </div>
 
         <div className='box-repos'>
+          <fieldset>
+            <legend>Repositórios</legend>
+            <ul id='ul-repos'>
+            </ul>
+          </fieldset>
         </div>
+
       </div>
     </>
   )
